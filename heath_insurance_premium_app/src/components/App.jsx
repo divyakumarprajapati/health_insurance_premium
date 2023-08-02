813b9dc8af5380859e01870ac05ba6e69dc8348e
divyakumarprajapati <divyakumarprajapati1@gmail.com>
2023-08-03 01:23:23 +0530

    added dynamic form

--- heath_insurance_premium_app/src/components/App.jsx
+++ heath_insurance_premium_app/src/components/App.jsx
@@ -1,14 +1,17 @@
-import './App.css';
-import Header from './Header';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import "./App.css";
+import Header from "./Header";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import PremiumForm from "./FamilyDetailsForm/FamilyDetailsForm";
 
-function App(){
+function App() {
   return (
     <div className="App">
       <Header />
       <Row>
-        <Col>1 of 2</Col>
+        <Col>
+          <PremiumForm />
+        </Col>
         <Col>2 of 2</Col>
       </Row>
     </div>
--- heath_insurance_premium_app/src/components/Header.jsx
+++ heath_insurance_premium_app/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
-    return (
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">Get Premium</Navbar.Brand>
-        </Container>
-      </Navbar>
-    )
-}
+  return (
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Get Premium</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Header
+export default Header;
